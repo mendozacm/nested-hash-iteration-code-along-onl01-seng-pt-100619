3ffd96561507bb75f17a89a2bce8f598f206b18e
Cesar Mendoza <mendoza.cesar92@gmail.com>
2019-10-24 20:18:54 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -13,15 +13,4 @@ require 'pry'
   #     favorite_ice_cream_flavors: ["strawberry", cookie dough", "mint chip"]
   #   }
   # }
-  contacts.each do |person, contact_details_hash|
-  if person == "Freddy Mercury"
-    contact_details_hash.each do |attribute, data|
-      binding.pry
-    end
-  end
-end
-def remove_strawberry(contacts)
-contacts.each do |person, contact_details_hash|
-  binding.pry
-end
-end
+  
